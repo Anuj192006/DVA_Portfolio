3f53aa5e6808fb4207b7b453f15b9a7ba41f1869
Anuj192006 <anuju712@gmail.com>
2026-04-28 21:14:44 +0530

    fix: update vite base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/DVA_PORFOLIO/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
